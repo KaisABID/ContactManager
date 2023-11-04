b3f337dbe4ee8c4abec649f181f31392f022bb58
KAIS ABID <kaisbenabid@gmail.com>
2023-11-04 23:46:01 +0100

    Initial commit

---
+++ Controllers/UsersController copy.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ContactManager.Models;
+
+namespace ContactManager.Controllers;
+
+public class UsersController : Controller
+{
+    private readonly ILogger<UsersController> _logger;
+    private MyContext _context;
+    public UsersController(ILogger<UsersController> logger, MyContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
